cd74319127a1985c1aa1784a9978041e7c6297f1
evneet <nancymaan4@gmail.com>
2020-03-19 20:41:20 -0400

    user added in database

--- javascript/script.js
+++ javascript/script.js
@@ -1,47 +1,7 @@
-// $('.form').find('input, textarea').on('keyup blur focus', function (e) {
-//
-//   var $this = $(this),
-//       label = $this.prev('label');
-//
-// 	  if (e.type === 'keyup') {
-// 			if ($this.val() === '') {
-//           label.removeClass('active highlight');
-//         } else {
-//           label.addClass('active highlight');
-//         }
-//     } else if (e.type === 'blur') {
-//     	if( $this.val() === '' ) {
-//     		label.removeClass('active highlight');
-// 			} else {
-// 		    label.removeClass('highlight');
-// 			}
-//     } else if (e.type === 'focus') {
-//
-//       if( $this.val() === '' ) {
-//     		label.removeClass('highlight');
-// 			}
-//       else if( $this.val() !== '' ) {
-// 		    label.addClass('highlight');
-// 			}
-//     }
-//
-// });
-//
-// $('.tab a').on('click', function (e) {
-//
-//   e.preventDefault();
-//
-//   $(this).parent().addClass('active');
-//   $(this).parent().siblings().removeClass('active');
-//
-//   target = $(this).attr('href');
-//
-//   $('.tab-content > div').not(target).hide();
-//
-//   $(target).fadeIn(600);
-//
-// });
-createDatabase();
+
+// createDatabase();
+createObjectStores();
+
 
 
 function createDatabase(){
@@ -64,4 +24,109 @@ openRequest.onsuccess = function() {
     console.log("success called")
 };
 
+}
+
+function createObjectStores(){
+  let openRequest = indexedDB.open("DB_traveldatabase", 2);
+
+openRequest.onupgradeneeded = function() {
+
+console.log("upgrade called")
+
+// add table cide ---
+let db = openRequest.result;
+
+  if (!db.objectStoreNames.contains('userdata')) { // if there's no "books" store
+    db.createObjectStore('userdata', {keyPath: 'id'}); // create it
+  }
+
+
+};
+
+openRequest.onerror = function() {
+
+    console.log("error called")
+
+};
+
+openRequest.onsuccess = function() {
+  //let db = openRequest.result;
+
+    console.log("success called")
+
+
+};
+}
+
+function registerUser(){
+  let firstname = document.getElementById('fname').value;
+  let lastname = document.getElementById('lname').value;
+  let pass = document.getElementById('passwordsignup').value;
+
+  if(firstname == "" & lastname == "" & pass == ""){
+    alert("please fill the required feilds");
+  }else{
+
+
+
+  let openRequest = indexedDB.open("DB_traveldatabase", 2);
+  openRequest.onupgradeneeded = function() {
+     console.log("upgrade called")
+  };
+
+  openRequest.onerror = function() {
+    console.log("error called")
+  };
+
+  openRequest.onsuccess = function() {
+     let db = openRequest.result;
+
+        console.log("success called")
+
+        db.onversionchange = function() {
+        db.close();
+         };//ver change
+         // your code: add data to table
+         let transaction = db.transaction("userdata", "readwrite");
+         let t = transaction.objectStore("userdata");
+
+         let data={
+            id: firstname,
+            lastname: lastname,
+            password: pass,
+            requests: []
+
+
+         }
+
+
+         let req = t.add(data);
+
+         req.onsuccess = function() {
+
+
+            console.log(req.result)
+            console.log(data);
+
+         };
+
+         req.onerror = function() {
+                     console.log("Error", req.error);
+              }
+};
+
+
+openRequest.onblocked = function() {
+
+};
+document.getElementById('fname').value = ""
+document.getElementById('lname').value = ""
+document.getElementById('passwordsignup').value = ""
+var x = document.getElementById("msg");
+x.className = "show";
+setTimeout(function() {
+  x.className.replace("show", "");
+}, 30000);
+
+}
 }
